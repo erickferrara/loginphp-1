8d9e9f2453a273cf4e60e1216cd7d7253fade6a7
Erick F Bazan <erickferrara@yahoo.com.br>
2018-12-13 15:24:33 -0200

    Tela de login

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+    <title>Praticando login</title>
+</head>
+<body>
+    <div class="login-box">
+        <h1>Login</h1>
+
+        <div class="txt-box">
+            <i class="fa fa-user"></i>
+            <input type="text" placeholder="Usuário" name="login" />
+        </div>
+
+        <div class="txt-box">
+            <i class="fa fa-lock"></i>
+            <input type="password" placeholder="Senha" name="login" />
+        </div>
+
+        <button class="btn" type="submit" name="entrar">Entrar</button>
+    </div>
+</body>
+</html>
